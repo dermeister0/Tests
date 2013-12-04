d06fda5d5b350eda9c2a2b7538c64d3fc1198d94
Der_Meister <der@heavysoft.ru>
2013-12-04 23:11:41 +0800

    Added TestApp.

---
+++ TestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new UsefulStuff.Test();
+            test.PrintVersion();
+        }
+    }
+}
