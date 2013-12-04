25163011ef2b9f356afbe4519da86c554fef74f2
Der_Meister <der@heavysoft.ru>
2013-12-04 23:02:26 +0800

    Added UsefulStuff10.

---
+++ UsefulStuff10/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+
+namespace UsefulStuff
+{
+    public class Test
+    {
+        public void PrintVersion()
+        {
+            Console.WriteLine("UsefulStuff Version = {0}", (Assembly.GetExecutingAssembly()).ToString());
+        }
+    }
+}
