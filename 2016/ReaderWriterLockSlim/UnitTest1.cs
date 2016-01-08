ef8878b1060f27e28828d9577d2d8846e6ccb0b2
Der_Meister <der@heavysoft.ru>
2016-01-09 00:55:33 +0700

    Add ReaderWriterLockSlim test project.

---
+++ 2016/ReaderWriterLockSlim/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReaderWriterLockSlimTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(LockRecursionException))]
+        public void TestMethod1()
+        {
+            var slimLock = new ReaderWriterLockSlim();
+            slimLock.EnterReadLock();
+            slimLock.EnterReadLock();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LockRecursionException))]
+        public void TestMethod2()
+        {
+            var slimLock = new ReaderWriterLockSlim();
+            slimLock.EnterReadLock();
+            slimLock.EnterWriteLock();
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var slimLock = new ReaderWriterLockSlim();
+            slimLock.EnterUpgradeableReadLock();
+            slimLock.EnterReadLock();
+
+            Assert.AreEqual(true, slimLock.IsReadLockHeld);
+            Assert.AreEqual(false, slimLock.IsWriteLockHeld);
+            Assert.AreEqual(true, slimLock.IsUpgradeableReadLockHeld);
+
+            slimLock.ExitReadLock();
+
+            Assert.AreEqual(false, slimLock.IsReadLockHeld);
+            Assert.AreEqual(false, slimLock.IsWriteLockHeld);
+            Assert.AreEqual(true, slimLock.IsUpgradeableReadLockHeld);
+
+            slimLock.EnterWriteLock();
+            Assert.AreEqual(false, slimLock.IsReadLockHeld);
+            Assert.AreEqual(true, slimLock.IsWriteLockHeld);
+            Assert.AreEqual(true, slimLock.IsUpgradeableReadLockHeld);
+            slimLock.ExitWriteLock();
+
+            Assert.AreEqual(false, slimLock.IsReadLockHeld);
+            Assert.AreEqual(false, slimLock.IsWriteLockHeld);
+            Assert.AreEqual(true, slimLock.IsUpgradeableReadLockHeld);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LockRecursionException))]
+        public void TestMethod4()
+        {
+            var slimLock = new ReaderWriterLockSlim();
+            slimLock.EnterUpgradeableReadLock();
+            slimLock.EnterUpgradeableReadLock();
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var slimLock = new ReaderWriterLockSlim();
+            slimLock.EnterUpgradeableReadLock();
+            slimLock.EnterReadLock();
+
+            Assert.AreEqual(true, slimLock.IsReadLockHeld);
+            Assert.AreEqual(false, slimLock.IsWriteLockHeld);
+            Assert.AreEqual(true, slimLock.IsUpgradeableReadLockHeld);
+
+            slimLock.ExitUpgradeableReadLock();
+
+            Assert.AreEqual(true, slimLock.IsReadLockHeld);
+            Assert.AreEqual(false, slimLock.IsWriteLockHeld);
+            Assert.AreEqual(false, slimLock.IsUpgradeableReadLockHeld);
+        }
+    }
+}
